1c3fa38fe9b1e1ea13ee15d4d2f108ea8a0f6971
agifsofyan <alpenizme@gmail.com>
2023-03-27 11:47:23 +0700

    Provider config

--- src/Providers/NotoServiceProvider.php
+++ src/Providers/NotoServiceProvider.php
@@ -40,7 +40,7 @@ class NotoServiceProvider extends ServiceProvider
             
             $this->publishes([
                 __DIR__.'/../config/noto.php' => config_path('noto.php'),
-              ], 'config');
+              ], 'noto-config');
         }
     }
 
@@ -52,11 +52,11 @@ class NotoServiceProvider extends ServiceProvider
     protected function migratePublishing()
     {
         if ($this->app->runningInConsole()) {
-            $migrationFileName = '2023_01_31_000001_Db_System_Files.php';
+            $migrationFileName = 'Db_System_Files';
             if (! $this->migrationFileExists($migrationFileName)) {
                 $this->publishes([
-                    __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
-                ], 'migrations');
+                    __DIR__ . "/../database/migrations/{$migrationFileName}.php" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
+                ], 'systemfiles-migrations');
             }
         }
     }
